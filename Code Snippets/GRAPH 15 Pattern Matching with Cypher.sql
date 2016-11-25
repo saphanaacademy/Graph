c00d57aa98d2f692d870dddd0813b543d49aa8f4
SAP HANA Academy <hanaacademy@sap.com>
2016-11-25 14:04:18 +0100

    HANA 2.0 SPS00
    HANA 2.0 SPS00

--- Code Snippets/GRAPH 15 Pattern Matching with Cypher.sql
+++ Code Snippets/GRAPH 15 Pattern Matching with Cypher.sql
@@ -8,14 +8,15 @@ CREATE CALCULATION SCENARIO "MATCH_SUBGRAPHS_EXAMPLE" USING '
 	<graph name="match_subgraphs_node" defaultViewFlag="true" schema="GREEK_MYTHOLOGY" workspace="GRAPH" action="MATCH_SUBGRAPHS">
 		<expression>
 			<![CDATA[
-				MATCH (A)-[E1]->(B), (A)-[E2]->(C), (B)-[E3]->(C)
+				MATCH (A)-[E1]->(B), (A)-[E2]->(C)
 				WHERE A.NAME < B.NAME AND E1.TYPE = ''marriedTo'' AND C.TYPE = ''god'' AND C.RESIDENCE = ''Olymp''
-				RETURN A.NAME AS PARENT1_NAME, B.NAME AS PARENT2_NAME, C.NAME
+				RETURN A.NAME AS PARENT1_NAME, B.NAME AS PARENT2_NAME, C.NAME AS CHILD_NAME
 			]]>
 		</expression>
 		<viewAttributes>
 			<viewAttribute name="PARENT1_NAME" datatype="string"/>
 			<viewAttribute name="PARENT2_NAME" datatype="string"/>
+			<viewAttribute name="CHILD_NAME" datatype="string"/>
 		</viewAttributes>
 	</graph>
    </calculationViews>
